4b160a376ca16d92db21a61d652daf5fc97d1fe8
Tostilocos <146779573+JesusCoe@users.noreply.github.com>
2026-01-21 17:52:13 +0000

    Added major changes to logic in script.js

--- script.js
+++ script.js
@@ -22,6 +22,23 @@ let computerScore = 0;
             const computerChoice = choices.get(randomInteger);
             return computerChoice; //returns the value assigned to that key, in this case PPT
         }
+
+        playerOptions.forEach(option =>{
+            option.addEventListener('click', function(){
+                winner(this.innerText, getComputerChoice());
+            })
+        })
+        
+    }
+
+    const winner = (player, computer) => {
+        const result = document.querySelector('result');
+        const playerScoreBoard = document.querySelector('.p-count');
+        const computerScoreBoard = document.querySelector('.c-count');
+
+        if(player === computer){
+            result.textContent('Tie!, you both picked ${computer}')
+        }
     }
 
 }
